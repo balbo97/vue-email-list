76faa2c655a05c047bf2ecbe7da5bb0cf6c73ae8
balbo97 <balbosauro1@gmail.com>
2023-12-20 16:17:30 +0100

    esercizio completed

--- js/script.js
+++ js/script.js
@@ -2,7 +2,18 @@ const{createApp} = Vue;
 createApp({
     data() {
         return {
-            
+            emailArray: [],
         }
+    },
+    created() {
+        for(let i=0; i<10; i++){
+            
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response)=>{
+                this.emailArray.push(response.data.response);
+            })
+        }
+    },
+    methods: {
+        
     },
 }).mount('#app')
