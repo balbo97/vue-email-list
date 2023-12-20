ab68c6e42f6d7e38b88b48924b7bdf2cea4aec36
balbo97 <balbosauro1@gmail.com>
2023-12-20 16:42:19 +0100

    BONUS completed

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ createApp({
             
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response)=>{
                 this.emailArray.push(response.data.response);
+                console.log(this.emailArray.length)
             })
         }
     },
